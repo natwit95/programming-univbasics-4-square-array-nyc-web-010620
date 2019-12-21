a63abd7afbef4ce44ae7134f7de632a4350978c3
Natalia  Wit <natwit95@gmail.com>
2019-12-21 18:15:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
-count = 0
-
-while count < array.length do
-    puts array[count]
-    count ** = 1
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
   end
+  squared
 end
